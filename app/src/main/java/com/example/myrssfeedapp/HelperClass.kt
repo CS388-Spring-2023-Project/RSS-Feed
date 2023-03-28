25f56d160544ac86d3359b85b93b2be3df6cfaba
Reda-AG7 <ragourram.7@gmail.com>
2023-03-27 22:04:13 -0400

    user_signIn_signUp

---
+++ app/src/main/java/com/example/myrssfeedapp/HelperClass.kt
@@ -0,0 +1,33 @@
+package com.example.myrssfeedapp
+
+import android.widget.EditText
+
+class HelperClass {
+    val backendURL = "http://192.168.1.7/RssFeed/backend.php"
+    //check if a field is empty
+    fun emptyField(field: EditText) : Boolean{
+        if(field.text.isEmpty()){
+            field.error = "${field.hint} cannot be empty"
+            return true
+        }
+        return false
+    }
+    //Check if email is valid
+    fun isValidEmail(emailContainer:EditText) : Boolean{
+        val emailPattern = "[a-zA-Z0-9._-]+@[a-z]+\\.[a-z]+"
+        return if(emailContainer.text.matches(emailPattern.toRegex())) true
+        else{
+            emailContainer.error = "${emailContainer.hint} is not a valid email"
+            false
+        }
+    }
+    //check if password and cPassword are matching
+    fun areIdentical(passwordContainer:EditText,
+                    cPasswordContainer:EditText) :Boolean{
+        return if(passwordContainer.text.toString() == cPasswordContainer.text.toString()) true
+        else{
+            cPasswordContainer.error = "password and its confirmation are not identical"
+            return false
+        }
+    }
+}
